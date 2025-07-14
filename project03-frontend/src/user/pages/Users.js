48386609fd99f8023bcaeb2ffc426b989d46d4c3
root <root@AIMIT668.>
2025-07-14 12:06:29 -0400

    update on 7.14.2025

--- project03-frontend/src/user/pages/Users.js
+++ project03-frontend/src/user/pages/Users.js
@@ -8,7 +8,7 @@ const Users = () => {
       id: 'u1',
       name: 'Jimmy Z',
       image:
-        'https://static.wikia.nocookie.net/thepokemonshow/images/8/80/20101007155439%21Ash_Pikachu.png/revision/latest?cb=20140425213328',
+        'https://s.yimg.com/ny/api/res/1.2/iazCdz6X9kTDrN7FPwVOvQ--/YXBwaWQ9aGlnaGxhbmRlcjt3PTk2MDtoPTk2MDtjZj13ZWJw/https://media.zenfs.com/en/us.finance.gurufocus/528af7acf1b685a79f0a0ee8c34ab69c',
       places: 3
     }
   ];
