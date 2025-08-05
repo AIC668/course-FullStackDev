06546e90021411abf1371b89a1ee5e3306acc433
AIT668 <ait668@AIMIT668>
2025-08-05 11:12:12 -0400

    86 adding express.js

--- prorject04-backend/app.js
+++ prorject04-backend/app.js
@@ -1,11 +1,28 @@
-const http = require('http');
+const express = require('express');
 
-const server = http.createServer((req, res) => {
-  console.log('INCOMING REQUEST');
-  console.log(req.method, req.url);
+const app = express();
 
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Success!</h1>');
+app.use((req, res, next) => {
+  let body = '';
+  req.on('end', () => {
+    const userName = body.split('=')[1];
+    if (userName) {
+      req.body = { name: userName };
+    }
+    next();
+  });
+  req.on('data', chunk => {
+    body += chunk;
+  });
 });
 
-server.listen(5000);
+app.use((req, res, next) => {
+  if (req.body) {
+    return res.send('<h1>' + req.body.name + '</h1>');
+  }
+  res.send(
+    '<form method="POST"><input type="text" name="username"><button type="submit">Create User</button></form>'
+  );
+});
+
+app.listen(5000);
